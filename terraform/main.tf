5419a0115a43d72ca4d558b3a6ab98c8590eecd5
Alan Bruhns <alanbruhns@gmail.com>
2024-09-16 10:56:17 -0300

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = "~/.kube/config"
+    config_context = "minikube"
+  }
+}
+
+# Install ArgoCD using Helm
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "5.41.1"  # Specify the ArgoCD version you want to install
+
+  # Create the namespace if it doesn't exist
+  create_namespace = true
+}
+
+# Create the ArgoCD Application resource after ArgoCD is installed
+resource "kubernetes_manifest" "argo_application" {
+  depends_on = [helm_release.argocd]
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "app-of-apps"
+      namespace = "argocd"
+      finalizers = ["resources-finalizer.argocd.argoproj.io"]
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://github.com/alanbruhns/ldp-platform-helm"
+        targetRevision = "main"
+        path           = "app-of-apps"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "argocd"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+      }
+    }
+  }
+}
